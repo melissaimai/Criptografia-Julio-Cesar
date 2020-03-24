31dc825b110e6f13e39833317a51ea23b40e3eb2
Melissa Yuri Campos Imai <40413924@redecorp.br>
2020-03-23 21:37:52 -0300

    commit inicial

---
+++ Criptografa.java
@@ -0,0 +1,25 @@
+
+public class Criptografa {
+	 
+	public static void main(String [] args) {
+		String criptografada = "sr xli mrxivrix, rsfshc orsaw csy evi e hsk. tixiv wximriv";
+		int casas = 4;				
+				
+			  String criptogradafa = criptografada.toLowerCase();
+				char[] charArray = criptografada.toCharArray();
+				for (char c : charArray) {
+					String d = String.valueOf(c);
+					int numeroAscii = d.charAt(0);
+						if(numeroAscii >= 101) {
+							numeroAscii = numeroAscii - casas;
+						}else if (numeroAscii < 101 && numeroAscii >= 97) {
+							numeroAscii = numeroAscii + 22;
+						}else {
+							numeroAscii = numeroAscii;
+						}
+					String novo = new Character((char)numeroAscii).toString();
+					System.out.print(novo);
+				}
+				
+			}
+		}
